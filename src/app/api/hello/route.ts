ee75df9c23455de4396502b2624a895cc5017e28
Nicolas <villagrajosenicolas@gmail.com>
2024-08-10 18:01:37 -0300

    update

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,6 +1,6 @@
 // app/api/hello/route.js
 
-export async function GET(request) {
+export async function GET(request:any) {
     return new Response(JSON.stringify({ message: 'Hello, world!' }), {
       headers: { 'Content-Type': 'application/json' },
     });
