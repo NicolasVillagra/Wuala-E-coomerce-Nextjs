544a272f1354fd2c9c25a7dc386ec5fc19c2c582
Nicolas <villagrajosenicolas@gmail.com>
2024-08-13 15:47:58 -0300

    update

--- src/services/mongodb.ts
+++ src/services/mongodb.ts
@@ -1,8 +1,13 @@
 import mongoose from 'mongoose';
 
-
 async function connectToDatabase() {
- await mongoose.connect(process.env.MONGODB_URI)
+  const mongoUri = process.env.MONGODB_URI;
+  
+  if (!mongoUri) {
+    throw new Error('El URI de MongoDB no está definido en las variables de entorno');
+  }
+
+  await mongoose.connect(mongoUri);
 }
 
 export default connectToDatabase;
